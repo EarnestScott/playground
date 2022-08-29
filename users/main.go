277af4eea972c592b6019df14d0e009a285465cc
Earnest Scott <earnestdscott@gmail.com>
2022-08-28 19:18:28 -0700

    basic user auth w jwt

---
+++ users/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/EarnestScott/Playground/users/controllers"
+	"github.com/EarnestScott/Playground/users/database"
+	"github.com/EarnestScott/Playground/users/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database.Connect("host=localhost user=postgresUser password=postgresPW dbname=postgresDB port=5455 sslmode=disable TimeZone=America/Los_Angeles")
+	database.Migrate()
+	router := initRouter()
+	router.Run(":8080")
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.POST("/token", controllers.GenerateToken)
+		api.POST("/user/register", controllers.RegisterUser)
+		secured := api.Group("/secured").Use(middlewares.Auth())
+		{
+			secured.GET("/ping", controllers.Ping)
+		}
+	}
+	return router
+}
