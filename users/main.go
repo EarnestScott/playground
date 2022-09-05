4c4f167c197a5d45ab7bbdf396d34184712ec20a
Earnest Scott <earnestdscott@gmail.com>
2022-09-05 14:08:00 -0700

    finally got elastic beanstalk to work gd

--- users/main.go
+++ users/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"net/http"
 	"os"
 
 	"github.com/EarnestScott/playground/users/controllers"
@@ -21,20 +22,6 @@ func main() {
 	port := os.Getenv("PORT")
 
 	conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUserName, dbPass, dbHostName, dbPort, dbName)
-	// conn := flag.String("conn", "", "Specify db connection string")
-
-	// // Declare a flag called age with default value of 0 and a help message
-	// port := flag.Int("port", 0, "Specify connection port")
-
-	// // Enable command-line parsing
-	// flag.Parse()
-
-	// if *conn == "" {
-	// 	panic("No connection string specified")
-	// }
-	// if *port == 0 {
-	// 	panic("No port number specified")
-	// }
 
 	portStr := fmt.Sprintf(":%s", port)
 	database.Connect(conn)
@@ -45,6 +32,9 @@ func main() {
 
 func initRouter() *gin.Engine {
 	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello")
+	})
 	api := router.Group("/api")
 	{
 		api.POST("/token", controllers.GenerateToken)
